77b4d7c5b24c13ff7ce4a4e1543eb0a0ac07dac6
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-12-07 10:11:19 +0530

    reset field

--- src/pages/masterModule/Components/Service.tsx
+++ src/pages/masterModule/Components/Service.tsx
@@ -255,7 +255,10 @@ const Service = () => {
               <Button
                 // type="reset"
                 className="shadow bg-red-700 hover:bg-red-600 shadow-slate-500"
-                onClick={() => setResetModel(true)}
+                onClick={(e: any) => {
+                  setResetModel(true);
+                  e.preventDefault();
+                }}
               >
                 Reset
               </Button>
