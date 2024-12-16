7ba9f4add8c238798660c96bf071ed58e689957e
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-12-16 17:20:34 +0530

    remark in min register

--- src/pages/ReportsModule/MinRegister.tsx
+++ src/pages/ReportsModule/MinRegister.tsx
@@ -119,8 +119,8 @@ const MinRegister = () => {
       width: 280,
     },
     {
-      headerName: "Remark",
-      field: "COMMENT",
+      headerName: "Decription",
+      field: "Decription",
       filter: "agTextColumnFilter",
       cellRenderer: CopyCellRenderer,
       width: 250,
@@ -237,6 +237,13 @@ const MinRegister = () => {
       field: "ISSUEBY",
       filter: "agTextColumnFilter",
       width: 190,
+    },
+    {
+      headerName: "Remark",
+      field: "COMMENT",
+      filter: "agTextColumnFilter",
+      cellRenderer: CopyCellRenderer,
+      width: 250,
     },
   ];
   const type = [
