a07b05599230dac6e371dd122b0e4d44b4039e51
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2025-01-15 15:30:07 +0530

    test tsconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+import tailwindScrollbar from "tailwind-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -47,6 +50,7 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shake: "shake 0.2s ease-out",
       },
       colors: {
         hbg: "#e0f2f1",
@@ -90,38 +94,9 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-        shake: {
-          "10%, 90%": {
-            transform: "translate3d(-1px, 0, 0)",
-          },
-          "20%, 80%": {
-            transform: "translate3d(2px, 0, 0)",
-          },
-          "30%, 50%, 70%": {
-            transform: "translate3d(-4px, 0, 0)",
-          },
-          "40%, 60%": {
-            transform: "translate3d(4px, 0, 0)",
-          },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        shake: "shake 0.2s ease-out",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
+  plugins: [tailwindcssAnimate, tailwindScrollbar],
   variants: {
     scrollbar: ["rounded"],
   },
